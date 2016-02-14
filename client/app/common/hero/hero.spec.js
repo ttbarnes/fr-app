235d15973891fdec813e53e404d1868857d115a6
ttbarnes <tony@tonybarnes.me>
2016-02-14 17:56:32 +0000

    fix: unit tests, tidy indentation

--- client/app/common/hero/hero.spec.js
+++ client/app/common/hero/hero.spec.js
@@ -4,69 +4,60 @@ import HeroComponent from './hero.component';
 import HeroTemplate from './hero.html';
 
 describe('Hero', ()=>{
-	let $rootScope,
-	makeController;
+  let $rootScope,
+  makeController;
 
-	beforeEach(window.module(HeroModule.name));
-	beforeEach(inject((_$rootScope_)=>{
-		$rootScope = _$rootScope_;
-		makeController = ()=>{
-			return new HeroController();
-		};
-	}));
+  beforeEach(window.module(HeroModule.name));
+  beforeEach(inject((_$rootScope_)=>{
+    $rootScope = _$rootScope_;
+    makeController = ()=>{
+      return new HeroController();
+    };
+  }));
 
-	describe('Module', ()=>{
-		// test things about the component module
-		// checking to see if it registers certain things and what not
-		// test for best practices with naming too
-		// test for routing
-	});
+  describe('Module', ()=>{
+    // test things about the component module
+    // checking to see if it registers certain things and what not
+    // test for best practices with naming too
+    // test for routing
+  });
 
-	describe('Controller', ()=>{
-		// test your controller here
+  describe('Controller', ()=>{
 
-		it('should have a name property [REMOVE]', ()=>{ // erase me if you remove this.name from the controller
-			let controller = makeController();
-
-			expect(controller).to.have.property('name');
-		});
-	});
-
-	describe('Template', ()=>{
-		// test the template
-		// use Regexes to test that you are using the right bindings {{  }}
-
-		it('should have name in template [REMOVE]', ()=>{
-			expect(HeroTemplate).to.match(/{{\s?vm\.name\s?}}/g);
-		});
-	});
-
-  describe('Component', () => {
-      // component/directive specs
-      let component = HeroComponent;
-
-      it('invokes transclude', () => {
-        expect(component.transclude).to.be.true
-      });
-
-      it('includes the intended template',() => {
-        expect(component.template).to.equal(HeroTemplate);
-      });
-
-      it('invokes the right controller', () => {
-        expect(component.controller).to.equal(HeroController);
-      });
-
-      it('uses `bindings`', () => {
-        expect(component).to.have.property('bindings')
-          .that.is.an('object')
-      });
-
-      it('includes the relevant `bindings`', () => {
-        expect(component.bindings).to.have.deep.property('title');
-        expect(component.bindings).to.have.deep.property('imgUrl');
-        expect(component.bindings).to.have.deep.property('imgAlt');
-      })
+    it('should have a name property [REMOVE]', ()=>{ // erase me if you remove this.name from the controller
+      let controller = makeController();
+      expect(controller).to.have.property('name');
+    });
 
   });
+
+  describe('Component', () => {
+
+    let component = HeroComponent;
+
+    it('invokes transclude', () => {
+      expect(component.transclude).to.be.true
+    });
+
+    it('includes the intended template',() => {
+      expect(component.template).to.equal(HeroTemplate);
+    });
+
+    it('invokes the right controller', () => {
+      expect(component.controller).to.equal(HeroController);
+    });
+
+    it('uses `bindings`', () => {
+      expect(component).to.have.property('bindings')
+        .that.is.an('object')
+    });
+
+    it('includes the relevant `bindings`', () => {
+      expect(component.bindings).to.have.deep.property('title');
+      expect(component.bindings).to.have.deep.property('imgUrl');
+      expect(component.bindings).to.have.deep.property('imgAlt');
+    })
+
+  });
+
 });
--- client/app/components/home/home.spec.js
+++ client/app/components/home/home.spec.js
@@ -4,50 +4,32 @@ import HomeComponent from './home.component';
 import HomeTemplate from './home.html';
 
 describe('Home', ()=>{
-	let $rootScope,
-	makeController;
+  let $rootScope,
+  makeController;
 
-	beforeEach(window.module(HomeModule.name));
-	beforeEach(inject((_$rootScope_)=>{
-		$rootScope = _$rootScope_;
-		makeController = ()=>{
-			return new HomeController();
-		};
-	}));
+  beforeEach(window.module(HomeModule.name));
+  beforeEach(inject((_$rootScope_)=>{
+    $rootScope = _$rootScope_;
+    makeController = ()=>{
+      return new HomeController();
+    };
+  }));
 
-	describe('Module', ()=>{
-		// test things about the component module
-		// checking to see if it registers certain things and what not
-		// test for best practices with naming too
-		// test for routing
-	});
+  describe('Component', ()=>{
+    let component = HomeComponent;
 
-	describe('Controller', ()=>{
-		// test your controller here
+    it('should use the right template',()=>{
+      expect(component.template).to.equal(HomeTemplate);
+    });
 
-		it('should have a name property [REMOVE]', ()=>{ // erase me if you remove this.name from the controller
-			let controller = makeController();
+    it('should use controllerAs', ()=>{
+      expect(component).to.have.property('controllerAs');
+    });
 
-			expect(controller).to.have.property('name');
-		});
-	});
-
-	describe('Component', ()=>{
-			// test the component/directive itself
-			let component = HomeComponent();
-
-			it('should use the right template',()=>{
-				expect(component.template).to.equal(HomeTemplate);
-			});
-
-			it('should use controllerAs', ()=>{
-				expect(component).to.have.property('controllerAs');
-			});
-
-			it('should use the right controller', ()=>{
-				expect(component.controller).to.equal(HomeController);
-			});
-	});
+    it('should use the right controller', ()=>{
+      expect(component.controller).to.equal(HomeController);
+    });
+  });
 });
 
 
