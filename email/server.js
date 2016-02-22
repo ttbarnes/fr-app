d4a8be9c76c12a4f8842e94dee0715c1be94eb6b
ttbarnes <tony@tonybarnes.me>
2016-02-22 15:47:02 +0000

    email: server, api POST endpoint using nodemailer

---
+++ email/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var app = express();
+var methodOverride = require('method-override');
+var nodemailer = require('nodemailer');
+var port = process.env.PORT || 2000;
+
+app.use(function (req, res, next){
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //allow client server access
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  methodOverride('X-HTTP-Method-Override')
+  bodyParser.urlencoded({ extended: true })
+  next();
+
+});
+
+//create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport('smtps://tonybarnes1408%40gmail.com:u46wnyUmaEs4hNT@smtp.gmail.com');
+
+app.post('/api/postEmail', bodyParser.json(), function (req, res) {
+
+    console.log('------ Email POST request at ' + new Date() + '\n' + 'req.body: ')
+    console.log('name: ' + req.body.name)
+    console.log('email: ' + req.body.email)
+    console.log('message: ' + req.body.message)
+
+    var mailData = {
+      from: req.body.name + ' ' + req.body.email,
+      //to: 'fiona@fionaross.co.uk',
+      to: 'tony@tonybarnes.me',
+      subject: 'Fiona Ross web app enquiry',
+      html: '<div style="background:#121212;color:#FFF;padding:2%;">Fiona Ross web app enquiry</div><br/>' + '<div style="padding:2%;"><p><b>From: </b><br/>'  + req.body.name + ' (' + req.body.email + ')</p></div>'  + '<br/><div style="padding:0 2%;"><b>Enquiry:</b></div>' + '<div style="padding:0 2%;">' + req.body.message + '</div>'
+    };
+
+    transporter.sendMail(mailData, function (err, info){
+        if(err){
+          return console.log('Error: ' + err);
+          res.send(err)
+        }
+        res.json({ message: 'Email sent!'})
+        console.log('Email sent! ' + info.response);
+    });
+
+});
+
+app.listen(port);
+console.log('Server running on port ' + port);
