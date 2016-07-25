1713c59bb7b5e8de0dc93a9bad91d240b269bebc
ttbarnes <tony@tonybarnes.me>
2016-07-25 11:00:03 +0100

    contact: form validation

--- client/app/components/contact/contact.controller.js
+++ client/app/components/contact/contact.controller.js
@@ -1,14 +1,26 @@
 import emailService from '../../services/email.service';
 
 class ContactController {
-  constructor(emailService) {
+  constructor(emailService, $scope) {
     "ngInject";
 
     this.emailService = emailService
+    this.$scope = $scope;
+
+    this.messages = {
+      error: false,
+      success: false
+    };
 
     this.sendMail = (user) => {
-      return this.emailService.send(user)
-    }
+      return this.emailService.send(user).then(() => {
+        this.$scope.vm.messages.error = false;
+        this.$scope.vm.messages.success = true;
+      }, (err) => {
+        this.$scope.vm.messages.error = true;
+      });
+    };
+
 
   }
 }
