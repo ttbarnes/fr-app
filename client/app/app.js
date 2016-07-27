a31524abf5ad6264d479fd35affa4451527837d5
ttbarnes <tony@tonybarnes.me>
2016-07-27 16:32:14 +0100

    global: view fadeIn on

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import ngAnimate from 'angular-animate';
 import Common from './common/common';
 import Components from './components/components';
 import Services from './services/services';
@@ -8,15 +9,26 @@ import 'normalize.css';
 
 angular.module('app', [
     uiRouter,
+    ngAnimate,
     Common.name,
     Components.name,
     Services.name
   ])
+
   .config(($locationProvider, $urlRouterProvider, plangularConfigProvider) => {
     'ngInject';
     $locationProvider.html5Mode(true).hashPrefix('!');
-    $urlRouterProvider.otherwise("/error");
+
+    $urlRouterProvider.otherwise('/error');
+
     plangularConfigProvider.clientId = '209756cdccc1f718176cea87c3b77f99';
+  })
+
+  .run(($rootScope) => {
+    'ngInject';
+    $rootScope.$on('$stateChangeSuccess', () => {
+       document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
   })
 
   .component('app', AppComponent);
