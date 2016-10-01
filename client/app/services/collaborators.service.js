0852ccf0432a5caa22810dab8c83e23972d11f67
ttbarnes <tony@tonybarnes.me>
2016-10-01 18:43:11 +0100

    collaborators: link to individual state

--- client/app/services/collaborators.service.js
+++ client/app/services/collaborators.service.js
@@ -3,6 +3,9 @@ import collaborators from '../data/collaborators.json';
 export default class collaboratorsService {
 
   constructor(){
+    angular.forEach(collaborators, (c, i) => {
+      c.id = i + 1;
+    });
     this.collaborators = collaborators;
   }
 
