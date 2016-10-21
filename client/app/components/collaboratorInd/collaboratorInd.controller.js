9f219d1a92d89f0334272931b3bf1f4b3507e225
ttbarnes <tony@tonybarnes.me>
2016-10-21 21:22:58 +0100

    collaboratorInd - next collab nav

--- client/app/components/collaboratorInd/collaboratorInd.controller.js
+++ client/app/components/collaboratorInd/collaboratorInd.controller.js
@@ -2,9 +2,15 @@ class CollaboratorIndController {
   constructor($state, $stateParams, collaboratorsService) {
     'ngInject';
     this.stateParams = $stateParams;
+    const currentId = this.stateParams.id;
     this.collaboratorsService = collaboratorsService;
 
-    this.collab = this.collaboratorsService.getSingle(this.stateParams.id);
+    this.collab = this.collaboratorsService.getSingle(currentId);
+
+    this.collabState = {
+      detail: this.collaboratorsService.getPrevNextCollab(currentId),
+      onLast: this.collaboratorsService.isLastCollaborator(currentId)
+    };
 
   }
 }
--- client/app/services/collaborators.service.js
+++ client/app/services/collaborators.service.js
@@ -15,6 +15,49 @@ export default class collaboratorsService {
       return this.collaborators[id];
     }
 
+    this.firstCollaborator = () => {
+      const c = collaborators[0],
+            obj = {
+              id: c.id,
+              name: c.name
+            };
+      return obj;
+    };
+
+    this.isFirstCollaborator = (id) => {
+      const c = this.firstCollaborator();
+      if (id === c.id) {
+        return true;
+      }
+      return false;
+    }
+
+    this.isLastCollaborator = (id) => {
+      const collabs = collaborators;
+      const lastCollab = collaborators[collaborators.length - 1].id;
+      if (id >= lastCollab) {
+        return true;
+      }
+      return false;
+    }
+
+    this.getPrevNextCollab = (id) => {
+      const currentId = parseInt(id, 10);
+            const prevCollab = this.isFirstCollaborator(currentId) ? false : this.collaborators[currentId - 2];
+            const nextCollab = this.isLastCollaborator(currentId) ? false : this.collaborators[currentId];
+            const collabState = {
+              prev: {
+                id: this.isFirstCollaborator(currentId) ? false : prevCollab.id,
+                name: this.isFirstCollaborator(currentId) ? false : prevCollab.name
+              },
+              next: {
+                id: this.isLastCollaborator(currentId) ? this.firstCollaborator().id : nextCollab.id,
+                name: this.isLastCollaborator(currentId) ? this.firstCollaborator().name : nextCollab.name
+              }
+            };
+      return collabState;
+    }
+
   }
 
 }
