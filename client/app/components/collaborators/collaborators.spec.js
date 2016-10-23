22f22ffe82710be0a0cfe027147ed1d994acd923
ttbarnes <tony@tonybarnes.me>
2016-10-23 15:37:48 +0100

    collaborators: unit tests

--- client/app/components/collaborators/collaborators.spec.js
+++ client/app/components/collaborators/collaborators.spec.js
@@ -2,24 +2,48 @@ import CollaboratorsModule from './collaborators'
 import CollaboratorsController from './collaborators.controller';
 import CollaboratorsComponent from './collaborators.component';
 import CollaboratorsTemplate from './collaborators.html';
+import Services from '../../services/services.js';
+import CollaboratorsService from '../../services/collaborators.service.js';
 
 describe('Collaborators', () => {
-  let $rootScope, makeController;
+  let $rootScope,
+      makeController,
+      collaboratorsService,
+      mockCollaboratorsService = {
+        collaborators : [
+          { name: 'test name'},
+          { name: 'another name'}
+        ]
+      };
 
-  beforeEach(window.module(CollaboratorsModule.name));
-  beforeEach(inject((_$rootScope_) => {
+  beforeEach(() => {
+    window.module(Services.name);
+    window.module(CollaboratorsModule.name);
+  });
+  beforeEach(inject((_$rootScope_, collaboratorsService) => {
     $rootScope = _$rootScope_;
+    collaboratorsService = mockCollaboratorsService;
     makeController = () => {
-      return new CollaboratorsController();
+      return new CollaboratorsController(mockCollaboratorsService);
     };
   }));
 
   describe('Controller', () => {
 
+    it('should inject collaborators service', () => {
+      let controller = makeController(collaboratorsService);
+      expect(controller.collaboratorsService).toBeDefined();
+    });
+
+    it('should have collaborators defined', () => {
+      let controller = makeController(collaboratorsService);
+      expect(controller.collaborators).toBeDefined();
+      expect(controller.collaborators).toEqual(jasmine.any(Array));
+    });
+
   });
 
   describe('Component', () => {
-      // component/directive specs
       let component = CollaboratorsComponent;
 
       it('includes the intended template',() => {
