f9c76743c8c66bf2d07ba653f145895641822454
ttbarnes <tony@tonybarnes.me>
2016-10-29 12:51:09 +0100

    home: one album only

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -3,8 +3,8 @@ import albumsService from '../../services/albums.service';
 class HomeController {
   constructor(albumsService) {
     'ngInject';
-    this.albumsService = albumsService
-    this.albums = albumsService.allAlbums
+    this.albumsService = albumsService;
+    this.latestAlbum = albumsService.allAlbums[0];
   }
 }
 
