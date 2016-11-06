a58704eb87ec9ea75761d3c24dcfd3514702bf8a
ttbarnes <tony@tonybarnes.me>
2016-11-06 17:34:17 +0000

    home: twitter feed

--- client/app/app.js
+++ client/app/app.js
@@ -7,6 +7,7 @@ import pageslide from 'angular-pageslide-directive';
 import modal from 'angular-ui-bootstrap';
 import bootstrapLightbox from 'angular-bootstrap-lightbox';
 import ezfb from 'angular-easyfb';
+import ngtweet from 'ngtweet';
 import angulartics from 'angulartics';
 import angularticsGA from 'angulartics-google-analytics';
 import Common from './common/common';
@@ -24,6 +25,7 @@ angular.module('app', [
     'ui.bootstrap.modal',
     'bootstrapLightbox',
     'ezfb',
+    'ngtweet',
     'angulartics',
     'angulartics.google.analytics',
     Common.name,
