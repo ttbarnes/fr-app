d4220ab7cb1d7c4b978ec9d708b1475215f689c5
ttbarnes <tony@tonybarnes.me>
2016-11-20 12:05:05 +0000

    update url references for prod

--- client/app/constants/constants.js
+++ client/app/constants/constants.js
@@ -1,5 +1,5 @@
 export const PLANGULAR_CLIENT_ID = '209756cdccc1f718176cea87c3b77f99';
 export const FB_ID = '1779328982334234';
-export const EMAIL_POST = 'https://fionaross.co.uk/api/postEmail';
-export const GALLERY_IMAGE_URL = 'https://fionaross.co.uk/images/gallery/';
+export const EMAIL_POST = 'http://fionaross.co.uk/api/postEmail';
+export const GALLERY_IMAGE_URL = 'http://fionaross.co.uk/images/gallery/';
 export const GALLERY_TEMPLATE_URL = '../app/components/gallery/modal.html'
