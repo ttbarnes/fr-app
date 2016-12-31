a1aa49876bbfbfae45dff62e0ce823230eaafb0f
ttbarnes <tony@tonybarnes.me>
2016-12-31 11:59:01 +0000

    supress state change console error message

--- client/app/app.js
+++ client/app/app.js
@@ -34,11 +34,13 @@ angular.module('app', [
     Services.name
   ])
 
-  .config(($locationProvider, $urlRouterProvider, plangularConfigProvider, ezfbProvider, LightboxProvider) => {
+  .config(($locationProvider, $urlRouterProvider, $qProvider, plangularConfigProvider, ezfbProvider, LightboxProvider) => {
     'ngInject';
     $locationProvider.html5Mode(true).hashPrefix('!');
 
     $urlRouterProvider.otherwise('/error');
+
+    $qProvider.errorOnUnhandledRejections(false); //prevents state change issue. see https://github.com/angular-ui/ui-router/issues/42#issuecomment-269367965
 
     plangularConfigProvider.clientId = CONST.PLANGULAR_CLIENT_ID;
 
