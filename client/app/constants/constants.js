a9f26235da2a5af5685ca051abb980e12277c314
ttbarnes <tony@tonybarnes.me>
2017-01-23 20:35:55 +0000

    latest album release date

--- client/app/constants/constants.js
+++ client/app/constants/constants.js
@@ -2,4 +2,4 @@ export const PLANGULAR_CLIENT_ID = '209756cdccc1f718176cea87c3b77f99';
 export const FB_ID = '1779328982334234';
 export const EMAIL_POST = 'http://www.fionaross.co.uk/api/postEmail';
 export const GALLERY_IMAGE_URL = 'http://fionaross.co.uk/images/gallery/';
-export const GALLERY_TEMPLATE_URL = '../app/components/gallery/modal.html'
+export const GALLERY_TEMPLATE_URL = '../app/components/gallery/modal.html';
