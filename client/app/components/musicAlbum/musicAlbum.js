1089077c5d2f45d10dc16bfd664dafa03df5092d
ttbarnes <tony@tonybarnes.me>
2017-02-19 10:57:39 +0000

    album ind page title with album name

--- client/app/components/musicAlbum/musicAlbum.js
+++ client/app/components/musicAlbum/musicAlbum.js
@@ -1,17 +1,25 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import musicAlbumComponent from './musicAlbum.component';
+import albumsService from '../../services/albums.service';
+import { PAGE_TITLE_MUSIC_ALBUM } from '../../constants/constants';
 
 let musicAlbumModule = angular.module('musicAlbum', [
   uiRouter
 ])
 
 .config(($stateProvider) => {
-  "ngInject";
+  'ngInject';
   $stateProvider
     .state('musicAlbum', {
       url: '/music/album/:id',
-      template: '<music-album></music-album>'
+      template: '<music-album></music-album>',
+      resolve: {
+        album: (albumsService, $stateParams) => {
+          return albumsService.getSingleAlbum($stateParams.id);
+        },
+        $title: (album) => album.name + PAGE_TITLE_MUSIC_ALBUM
+      }
     });
 })
 
