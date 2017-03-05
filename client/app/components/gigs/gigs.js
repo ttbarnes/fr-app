3cf773f60d32cb3ded015da8901138c82ba71333
ttbarnes <tony@tonybarnes.me>
2017-03-05 16:51:55 +0000

    gigs page title

--- client/app/components/gigs/gigs.js
+++ client/app/components/gigs/gigs.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import gigsComponent from './gigs.component';
+import { PAGE_TITLE_GIGS} from '../../constants/constants';
 
 let gigsModule = angular.module('gigs', [
   uiRouter
@@ -12,7 +13,10 @@ let gigsModule = angular.module('gigs', [
   $stateProvider
     .state('gigs', {
       url: '/gigs',
-      template: '<gigs></gigs>'
+      template: '<gigs></gigs>',
+      resolve: {
+        $title: () => PAGE_TITLE_GIGS
+      }
     });
 })
 
