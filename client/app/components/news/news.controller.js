f296f51c9ecc4566f49ec6dae7c89f747200a8a1
ttbarnes <tony@tonybarnes.me>
2017-03-12 21:49:03 +0000

    news data & style tweaks

--- client/app/components/news/news.controller.js
+++ client/app/components/news/news.controller.js
@@ -1,10 +1,12 @@
 import newsService from '../../services/news.service';
 
 class NewsController {
-  constructor(newsService) {
+  constructor(newsService, $sce) {
     'ngInject';
     this.newsService = newsService;
+    this.$sce = $sce;
     this.data = this.newsService.data;
+    this.trustSrc = (src) => this.$sce.trustAsResourceUrl(src);
   }
 }
 
