46c3ce4e763df852971af32c86bb73d5e89e5995
ttbarnes <tony@tonybarnes.me>
2017-03-24 18:51:01 +0000

    v1 music album press

--- client/app/components/musicAlbumPress/musicAlbumPress.controller.js
+++ client/app/components/musicAlbumPress/musicAlbumPress.controller.js
@@ -1,6 +1,14 @@
 class MusicAlbumPressController {
-  constructor() {
-    this.name = 'musicAlbumPress';
+  constructor($state, $stateParams, albumsService) {
+    'ngInject';
+    this.albumsService = albumsService;
+    this.stateParams = $stateParams;
+    this.currentId = this.stateParams.id || 2;
+
+    this.album = this.albumsService.getSingleAlbum(this.currentId);
+    if(!this.album) {
+      $state.go('error');
+    }
   }
 }
 
