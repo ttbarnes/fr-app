ee98fb2abd90b662805f3df66aa3a3640b06bd61
ttbarnes <tony@tonybarnes.me>
2017-03-25 07:44:43 +0000

    press page title

--- client/app/components/press/press.js
+++ client/app/components/press/press.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import pressComponent from './press.component';
+import { PAGE_TITLE_PRESS } from '../../constants/constants';
 
 let pressModule = angular.module('press', [
   uiRouter
@@ -11,7 +12,10 @@ let pressModule = angular.module('press', [
   $stateProvider
     .state('press', {
       url: '/press',
-      template: '<press></press>'
+      template: '<press></press>',
+      resolve: {
+        $title: () => PAGE_TITLE_PRESS
+      }
     });
 })
 
