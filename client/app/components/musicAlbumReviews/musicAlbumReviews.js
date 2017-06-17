35b6d7d439ab6ef87273b33d2e79089050fc1cd3
ttbarnes <tony@tonybarnes.me>
2017-06-17 10:16:01 +0100

    page title for music album reviews

--- client/app/components/musicAlbumReviews/musicAlbumReviews.js
+++ client/app/components/musicAlbumReviews/musicAlbumReviews.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import musicAlbumReviewsComponent from './musicAlbumReviews.component';
+import albumsService from '../../services/albums.service';
+import { PAGE_TITLE_MUSIC_ALBUM_REVIEWS } from '../../constants/constants';
 
 let musicAlbumReviewsModule = angular.module('musicAlbumReviews', [
   uiRouter
@@ -11,7 +13,13 @@ let musicAlbumReviewsModule = angular.module('musicAlbumReviews', [
   $stateProvider
     .state('musicAlbumReviews', {
       url: '/music/album/:id/reviews',
-      template: '<music-album-reviews></music-album-reviews>'
+      template: '<music-album-reviews></music-album-reviews>',
+      resolve: {
+        album: (albumsService, $stateParams) => {
+          return albumsService.getSingleAlbum($stateParams.id);
+        },
+        $title: (album) => album.name + PAGE_TITLE_MUSIC_ALBUM_REVIEWS
+      }
     });
 })
 
