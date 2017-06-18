ecc4393795372d97e0b7ad0361ac32fb05f45605
ttbarnes <tony@tonybarnes.me>
2017-06-18 10:57:10 +0100

    remove custom page title from musicAlbumReviews

--- client/app/components/musicAlbumReviews/musicAlbumReviews.js
+++ client/app/components/musicAlbumReviews/musicAlbumReviews.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import musicAlbumReviewsComponent from './musicAlbumReviews.component';
-import albumsService from '../../services/albums.service';
-import { PAGE_TITLE_MUSIC_ALBUM_REVIEWS } from '../../constants/constants';
 
 let musicAlbumReviewsModule = angular.module('musicAlbumReviews', [
   uiRouter
@@ -13,13 +11,7 @@ let musicAlbumReviewsModule = angular.module('musicAlbumReviews', [
   $stateProvider
     .state('musicAlbumReviews', {
       url: '/music/album/:id/reviews',
-      template: '<music-album-reviews></music-album-reviews>',
-      resolve: {
-        album: (albumsService, $stateParams) => {
-          return albumsService.getSingleAlbum($stateParams.id);
-        },
-        $title: (album) => album.name + PAGE_TITLE_MUSIC_ALBUM_REVIEWS
-      }
+      template: '<music-album-reviews></music-album-reviews>'
     });
 })
 
