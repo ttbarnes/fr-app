9dcec1d3f99a8f8977bb8659e09c5d719edd2cb2
ttbarnes <tony@tonybarnes.me>
2017-08-30 08:41:58 +0100

    v2 landing bg image conditions, responsive quotes

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,10 +1,28 @@
 import albumsService from '../../services/albums.service';
 
 class HomeController {
-  constructor(albumsService) {
+  constructor($window, $scope, albumsService) {
     'ngInject';
+    this.$window = $window;
     this.albumsService = albumsService;
     this.latestAlbum = albumsService.allAlbums[0];
+
+    this.$window.onscroll = () => {
+      const tabletScreenSize = window.matchMedia('(max-width: 1000px)').matches;
+      const largeScreenSize = window.matchMedia('(min-width: 1000px)').matches;
+      if (tabletScreenSize && document.body.scrollTop > 380) {
+        this.showBgImage = true;
+      } else if (largeScreenSize) {
+          this.showBgImage = false;
+        if (document.body.scrollTop > 430) {
+          this.showBgImage = true;
+        }
+      } else {
+        this.showBgImage = false;
+      }
+      $scope.$digest();
+    };
+
   }
 
 }
