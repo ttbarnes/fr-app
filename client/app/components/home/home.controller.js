caac43665964a8f7aa9d32a779526079e55c1e24
ttbarnes <tony@tonybarnes.me>
2017-09-22 19:51:15 +0100

    remove home bg animation

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -7,22 +7,6 @@ class HomeController {
     this.albumsService = albumsService;
     this.latestAlbum = albumsService.allAlbums[0];
 
-    this.$window.onscroll = () => {
-      const tabletScreenSize = window.matchMedia('(max-width: 1000px)').matches;
-      const largeScreenSize = window.matchMedia('(min-width: 1000px)').matches;
-      if (tabletScreenSize && document.body.scrollTop > 480) {
-        this.showBgImage = true;
-      } else if (largeScreenSize) {
-          this.showBgImage = false;
-        if (document.body.scrollTop > 430) {
-          this.showBgImage = true;
-        }
-      } else {
-        this.showBgImage = false;
-      }
-      $scope.$digest();
-    };
-
   }
 
 }
