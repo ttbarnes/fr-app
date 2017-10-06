d8ef6a2f7bcdc1690717737bc3e9fd6ec077cf50
ttbarnes <tony@tonybarnes.me>
2017-10-06 18:18:50 +0100

    signed photos design

--- client/app/components/gallerySignedPhoto/gallerySignedPhoto.controller.js
+++ client/app/components/gallerySignedPhoto/gallerySignedPhoto.controller.js
@@ -1,17 +1,15 @@
 class musicAlbumAudioCdController {
-  constructor($state, $stateParams, albumsService) {
-    'ngInject';
-    this.albumsService = albumsService;
-    this.stateParams = $stateParams;
-    this.currentId = this.stateParams.id || 2;
-
-    this.album = this.albumsService.getSingleAlbum(this.currentId);
-    if(!this.album) {
-      $state.go('error');
-    }
-    if (this.album && !this.album.canOrderAudioCd) {
-      $state.go('error');
-    }
+  constructor() {
+    this.photos = {
+      group1: [
+        { url: "./images/signed/1.jpg", alt:"Fiona Ross Signed Photo 1" },
+        { url: "./images/signed/2.jpg", alt:"Fiona Ross Signed Photo 2" }
+      ],
+      group2: [
+        { url: "./images/signed/3.jpg", alt:"Fiona Ross Signed Photo 3" },
+        { url: "./images/signed/4.jpg", alt:"Fiona Ross Signed Photo 4" }
+      ]
+    };
 
   }
 }
