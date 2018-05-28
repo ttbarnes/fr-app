6d124aa278929bef30b2e3b1be7aa69e7bc70f09
ttbarnes <tony@tonybarnes.me>
2018-05-28 10:01:06 +0100

    privacy policy

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -16,6 +16,7 @@ import Videos from './videos/videos';
 import NewsInd from './newsInd/newsInd';
 import Contact from './contact/contact';
 import Gigs from './gigs/gigs';
+import PrivacyPolicy from './privacyPolicy/privacyPolicy'
 import Error from './error/error';
 
 let componentModule = angular.module('app.components', [
@@ -36,6 +37,7 @@ let componentModule = angular.module('app.components', [
   NewsInd.name,
   Contact.name,
   Gigs.name,
+  PrivacyPolicy.name,
   Error.name
 ]);
 
