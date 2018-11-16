1b90b6f1b96435357c48d6ac08277274e6198421
ttbarnes <tony@tonybarnes.me>
2018-11-16 09:32:17 +0000

    new album placeholder

--- client/app/components/musicAlbum/musicAlbum.controller.js
+++ client/app/components/musicAlbum/musicAlbum.controller.js
@@ -4,7 +4,7 @@ class MusicAlbumController {
   constructor($state, $stateParams, albumsService) {
     'ngInject';
     this.stateParams = $stateParams;
-    this.currentId = this.stateParams.id || 2;
+    this.currentId = this.stateParams.id || 3;
     this.albumsService = albumsService;
 
     this.album = this.albumsService.getSingleAlbum(this.currentId);
