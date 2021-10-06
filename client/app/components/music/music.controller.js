56c830c95d9777d056543ff22202fa9ba6a37e9b
ttbarnes <tony@tonybarnes.me>
2021-10-06 09:37:38 +0100

    new album paypal link

--- client/app/components/music/music.controller.js
+++ client/app/components/music/music.controller.js
@@ -11,12 +11,7 @@ class MusicController {
     this.rootScope.ogTitle = PAGE_TITLE_MUSIC;
 
     this.redirectToAlbumPage = function(albumName) {
-      // var url = 'https://fionaross.co.uk/music/' + albumName;
-      // var url = 'http://google.com';
-      // var url = 'https://fr-react.netlify.app/music/Fierce-and-Non-Compliant';
       var url = 'https://fionaross.co.uk/music/album/' + albumName;
-      console.log('url ', url);
-      // window.location.replace(url);
       window.location = url;
     };
   }
