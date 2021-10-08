f110f254099731b58815b37f6765a69a21b8fa76
ttbarnes <tony@tonybarnes.me>
2021-10-08 11:04:29 +0100

    home links to new album

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -7,6 +7,12 @@ class HomeController {
     this.albumsService = albumsService;
     this.latestAlbum = albumsService.allAlbums[0];
     this.justMeAlbum = albumsService.allAlbums[1];
+
+    this.redirectToAlbumPage = function() {
+      var url = 'https://fionaross.co.uk/music/album/Red-Flags-and-High-Heels';
+      window.location = url;
+    };
+
   }
 
 }
